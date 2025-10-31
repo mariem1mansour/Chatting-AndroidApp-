bc6d2eaa2d5b3ce72063138a5f368e7b402206e2
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-10-31 19:16:08 +0100

    Remove google-services.json and add to .gitignore

--- app/src/main/java/mansourmariem/grp3/chattingapp/LoginOtpActivity.java
+++ app/src/main/java/mansourmariem/grp3/chattingapp/LoginOtpActivity.java
@@ -1,6 +1,8 @@
 package mansourmariem.grp3.chattingapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginOtpActivity extends AppCompatActivity {
+
+String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_otp);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+phoneNumber=getIntent().getExtras().getString("phone");
+        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_SHORT).show();
+
+
+
     }
 }
